9528791f2c73f672bb8e267a908ee15def7ea876
David Meinke <dmeinke@exchangetradedconcepts.com>
2022-07-06 08:55:23 -0500

    doc error

--- app/Controllers/Http/DocumentsController.ts
+++ app/Controllers/Http/DocumentsController.ts
@@ -1,7 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Document from "App/Models/Document";
 import FileUpload from "App/utils/fileUploader";
-//import {rules, schema} from "@ioc:Adonis/Core/Validator";
+import {rules, schema} from "@ioc:Adonis/Core/Validator";
 
 export default class DocumentsController {
   public async index({params, view}: HttpContextContract) {
@@ -13,11 +13,10 @@ export default class DocumentsController {
 
   public async create({}: HttpContextContract) {}
 
-  public async store({ /* request, response, auth, session,*/ view}: HttpContextContract) {
+  public async store({  request, response, auth, session}: HttpContextContract) {
 
     console.log('INSIDE STORE')
-    return view.render('maintenance/fund');
-    /*
+
 
     let dataUrl = ''
     let fileSize = 0
@@ -25,21 +24,21 @@ export default class DocumentsController {
     if (request.file('upload')) {
       const new_file = request.file('upload')
 
-      try {
         // @ts-ignore
         // console.log(new_file)
+      try {
         const data = await FileUpload.uploadToS3(request.file('upload'), 'uploads', null)
-        //console.log(data)
         dataUrl = data.url
         fileSize = data.stats.size
         fileType = new_file?.type
       }
-      catch (e) {
-        console.log(e)
-      }
+      catch (error){
+        console.log(error.original)
       }
 
+        //console.log(data)
 
+     }
 
     const dataS = await this.validateInput(request)
 
@@ -61,7 +60,7 @@ export default class DocumentsController {
     session.flash({notification: 'File Added'})
     return response.redirect('back')
 
-     */
+
   }
 
   public async show({}: HttpContextContract) {}
@@ -80,7 +79,6 @@ export default class DocumentsController {
 
   }
 
-/*
   private async validateInput(request) {
     const valSchema = schema.create({
       name: schema.string({trim: true}, [rules.maxLength(150), rules.required()]),
@@ -98,6 +96,5 @@ export default class DocumentsController {
       },
     })
   }
-*/
 
 }
