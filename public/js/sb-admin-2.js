65d6e07ff9dcaa00fd89bade7db99d37eba74f1b
David Meinke <dmeinke@exchangetradedconcepts.com>
2022-11-02 14:15:09 -0500

    fixed JS bug and disabled animate in logout

--- public/js/sb-admin-2.js
+++ public/js/sb-admin-2.js
@@ -15,7 +15,7 @@
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
     };
-    
+
     // Toggle the side navigation when window is resized below 480px
     if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
       $("body").addClass("sidebar-toggled");
@@ -35,6 +35,7 @@
   });
 
   // Scroll to top button appear
+  /*
   $(document).on('scroll', function() {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
@@ -43,7 +44,7 @@
       $('.scroll-to-top').fadeOut();
     }
   });
-
+*/
   // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function(e) {
     var $anchor = $(this);
